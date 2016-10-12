01957de6fa56d710505f423a7f41a907af29c9b2
29rou <c171401r@icu.ac.jp>
2016-10-12 19:11:19 +0900

    chage padded array func

--- main.cpp
+++ main.cpp
@@ -5,22 +5,22 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-std::vector<std::vector<float>> mat2array(const cv::Mat &src_img)
+std::vector<std::vector<bool>> mat2array(const cv::Mat &src_img)
 {
-  size_t row = src_img.rows;
-  size_t col = src_img.cols;
-  std::vector<std::vector<float>> img(row, std::vector<float>(col, 0));
+  size_t row = src_img.rows + 2;
+  size_t col = src_img.cols + 2;
+  std::vector<std::vector<bool>> img(row, std::vector<bool>(col, 0));
   int y, x;
 #pragma omp parallel for private(y, x) collapse(2)
-  for (y = 0; y < row; y++) {
-    for (x = 0; x < col; x++) {
-      img.at(y).at(x) = src_img.at<uint8_t>(y, x);
+  for (y = 1; y < row - 1; y++) {
+    for (x = 1; x < col - 1; x++) {
+      (src_img.at<uint8_t>(y - 1, x - 1) != 0) ? img.at(y).at(x) = 255 : 0;
     }
   }
   return img;
 }
 
-cv::Mat array2mat(const std::vector<std::vector<float>> array_img)
+cv::Mat array2mat(const std::vector<std::vector<bool>> array_img)
 {
   int row = array_img.size();
   int col = array_img.data()->size();
